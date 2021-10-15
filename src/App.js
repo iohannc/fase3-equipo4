6b05a06cfd844ed9ef4e8964b16a794fcbfcb150
Juan Negrete <juan.necastaneda@gmail.com>
2021-10-15 01:57:35 -0500

    Header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// Components
+import Header from './components/Header';
+// Styles
+import "../App.css"
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+// Assets
+import logo from "../../assets/images/logo.jpg";
+// Styles
+import { Wrapper, Logo, NavbarItems, Content } from "./Header.styles";
+
+const Header = () => {
+  return (
+    <Wrapper className="navbar is-fixed-top">
+    <Content className="container">
+      <div className="navbar-brand">
+        <Logo className="navbar-item" src={logo} alt="Logo" />
+      </div>
+      <NavbarItems className="navbar-menu is-active">
+        <div className="navbar-end">
+          <a href className="navbar-item">
+            ENTRAR
+          </a>
+          <a href className="navbar-item">
+            REGISTRO
+          </a>
+        </div>
+      </NavbarItems>
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default Header;
