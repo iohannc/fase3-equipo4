258c3ef22b88d37f8b5a2f584bd1d66521625182
Alex Aguilar <zhufany@gmail.com>
2021-10-16 04:43:57 -0500

    Agregado Categories componente para mostrar las categorias (tags)

---
+++ src/components/Categories/Categories.styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const CategoriesS= styled.div`
+    display: grid;
+    grid-template-columns: repeat(5,auto);
+    margin-top:auto;
+    a{
+        color: white;
+        font-family: 'Roboto', sans-serif;
+        text-align:center;
+    }
+`;
