ebdc65d4cc60917dbab85a49b7ab4a7480116a7b
HugoJosh <hugojoshua2000@comunidad.unam.mx>
2021-10-26 15:07:18 -0500

    Link a login

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,24 +3,25 @@ import React from "react";
 import logo from "../../assets/images/logo.jpg";
 // Styles
 import { Wrapper, Logo, NavbarItems, Content } from "./Header.styles";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Wrapper className="navbar">
-    <Content className="container">
-      <div className="navbar-brand">
-        <Logo className="navbar-item" src={logo} alt="Logo" />
-      </div>
-      <NavbarItems className="navbar-menu">
-        <div className="navbar-end">
-          <a href className="navbar-item">
-            ENTRAR
-          </a>
-          <a href className="navbar-item">
-            REGISTRO
-          </a>
+      <Content className="container">
+        <div className="navbar-brand">
+          <Logo className="navbar-item" src={logo} alt="Logo" />
         </div>
-      </NavbarItems>
+        <NavbarItems className="navbar-menu">
+          <div className="navbar-end">
+            <Link className="navbar-item" to="/login/">
+              ENTRAR
+            </Link>
+            <Link className="navbar-item" to="/">
+              REGISTRARSE
+            </Link>
+          </div>
+        </NavbarItems>
       </Content>
     </Wrapper>
   );
