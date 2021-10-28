f1d962e6ba3a5ca972ccd94a4f1ac2b768648f25
HugoJosh <hugojoshua2000@comunidad.unam.mx>
2021-10-27 21:23:42 -0500

    registro

--- src/components/Register/register2.js
+++ src/components/Register/register2.js
@@ -3,8 +3,9 @@ import React, { useEffect } from "react";
 import Header from "../Header/index2";
 import Banner from "../Banner/index2";
 import { GlobalStyle, Columnas, H2, H3, Bloque } from "../../GlobalStyle";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 function RegStep2() {
+  let history = useHistory();
   let datos = function () {
     const nombre = document.cookie.split('; ').find(row => row.startsWith('nombre=')).split('=')[1];
     const apellido = document.cookie.split('; ').find(row => row.startsWith('apellidos=')).split('=')[1];
@@ -14,6 +15,42 @@ function RegStep2() {
     const username1 = document.getElementById("username").innerHTML = username;
     const nombre1 = document.getElementById("nombre").innerHTML = nombre + " " + apellido;
     const email1 = document.getElementById("mail").innerHTML = email;
+  }
+  let continuar = async function () {
+    const url = "https://historiasdeterror.herokuapp.com/v1/usuarios/";
+    let result2 = {};
+    const nombre = document.cookie.split('; ').find(row => row.startsWith('nombre=')).split('=')[1];
+    const apellido = document.cookie.split('; ').find(row => row.startsWith('apellidos=')).split('=')[1];
+    const email = document.cookie.split('; ').find(row => row.startsWith('email=')).split('=')[1];
+    const password = document.cookie.split('; ').find(row => row.startsWith('password=')).split('=')[1];
+    const username = document.cookie.split('; ').find(row => row.startsWith('user=')).split('=')[1];
+    let cuerpo = { "administrador": "false", "username": username, "nombre": nombre, "apellido": apellido, "email": email, "password": password };
+    console.log(cuerpo)
+    let a = false;
+    await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify(cuerpo),
+    }).then(async (res) => await res.json()).then((res) => {
+      console.log(res);
+      a = true;
+      result2 = res;
+      let result = res.user;
+      document.cookie = `user=${result.username}`;
+      document.cookie = `token=${result2.token}`;
+      alert("Cuenta creada con exito");
+    }).catch((err) => {
+      console.log(err);
+    });
+    if (a) {
+      alert("Creacion de cuenta exitoso");
+      history.replace("/")
+    }
+    if (!a) {
+      alert("Creacion de cuenta fallido");
+    }
   }
   useEffect(() => {
     // Actualiza el título del documento usando la API del navegador
@@ -61,7 +98,7 @@ function RegStep2() {
               </H2>
             </Link>
           </button>
-          <button className="button is-rounded is-hovered is-small is-centered">
+          <button onClick={continuar} className="button is-rounded is-hovered is-small is-centered">
             Crear Cuenta
           </button>
         </div>
