7c1ee5caef06419c0545917ee59b9c04cf024eef
frasgado <frasgado@gmail.com>
2021-11-02 15:56:44 -0600

    Ajustes de leyendas e incorporar opciones de modificación y eliminación de historias

--- src/components/historia/index.js
+++ src/components/historia/index.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { Link } from 'react-router-dom';
+
 // Assets
 import { TextMain, TextSecond, TextTags, TextTagsMain, ContenedorText, TextSub } from "../../GlobalStyle";
 const MainHistoria = (props) => {
@@ -40,7 +42,57 @@ const MainHistoria = (props) => {
                     </div>
                 </div>
                 <div className="is-2">
-                    <TextMain>Iconos</TextMain>
+                    <span class="button is-info is-outlined">
+                    <i class="fas fa-pencil-alt"></i>    
+                    </span>
+                    &nbsp;
+                    <span class="button is-danger is-outlined">
+                    <i class="fas fa-trash"></i>    
+                    </span>
+                    {/* &nbsp;
+                    <span class="button is-success is-outlined">
+                    <i class="fas fa-check"></i>    
+                    </span>
+                    &nbsp;
+                    <span class="button is-outlined">
+                    <i class="fas fa-times"></i>    
+                    </span> */}
+
+
+                    {/* <TextMain>Iconos</TextMain>
+                    <i class="fas fa-trash"></i>
+                    <i class="fa fa-trash" aria-hidden="true"></i>
+                    <i class="fas fa-trash"></i>
+                    <i class="fas fa-pencil-alt"></i>
+
+                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                    <i class="fal fa-comment-alt-edit"></i>
+                    <i class="fas fa-comment-alt-edit"></i>
+                        <button class="button">
+                        <span class="icon is-small">
+                            <i class="fas fa-align-left"></i>
+                        </span>
+                        <span>Edit</span>
+                        </button>
+                        <button class="button is-danger is-outlined">
+                            <span>Delete</span>
+                            <span class="icon is-small">
+                            <i class="fas fa-times"></i>
+                            </span>
+                        </button>
+                    &nbsp;&nbsp;
+                    <button class="button is-success">
+                        <span class="icon is-small">
+                        <i class="fas fa-check"></i>
+                        </span>
+                        <span>Save</span>
+                    </button>
+                    <button class="button is-danger is-outlined">
+                            <span>Cancel</span>
+                            <span class="icon is-small">
+                            <i class="fas fa-times"></i>
+                            </span>
+                    </button> */}
                 </div>
             </div>
             <div className="columns is-centered is-narrow">
@@ -53,14 +105,17 @@ const MainHistoria = (props) => {
             <div className="columns is-centered">
                 <div className="column is-8">
                     <TextTagsMain>
-                        Tags (icono de tags)
+                        <span class="tag is-danger">
+                            <i class="fas fa-tag" is-medium></i>
+                        </span>
+                        Tags 
                     </TextTagsMain>
                 </div>
             </div>
             <div className="columns is-centered">
                 <div className="column is-8">
                     <TextTags id="tags">
-                        Tags
+                    Tags
                     </TextTags>
                 </div>
             </div>
