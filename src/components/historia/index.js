a2c848cb66db0726264782f2504cb003305d42dc
frasgado <frasgado@gmail.com>
2021-11-28 14:59:10 -0600

    Incorporar validaciones y edición de campos Titulo y Temática

--- src/components/historia/index.js
+++ src/components/historia/index.js
@@ -5,7 +5,22 @@ import { TextMain, TextSecond, TextTags, TextTagsMain, ContenedorText, TextSub }
 
 const MainHistoria = (props) => {
     let historia = useHistory();
+    const regresar = () => {
+        historia.push(`/`);
+        return true;
+    }
+
+    // let enviando = false;
     const cambiar = () => {
+        // if (!enviando) {
+        //     enviando= true;
+        //     historia.push(`/historia/${props.show}/edit`);
+        //     return true;
+        // } else {
+        //     //Si llega hasta aca significa que pulsaron 2 veces el boton submit
+        //     alert("El formulario ya se esta enviando");
+        //     return false;
+        // }
         historia.push(`/historia/${props.show}/edit`);
     }
     const entrar = () => {
@@ -33,7 +48,8 @@ const MainHistoria = (props) => {
         let token = document.cookie.split(';').find(row => row.trim().startsWith('token='));
         let id = document.cookie.split(';').find(row => row.trim().startsWith('id='))
         const url = `https://historiasdeterror.herokuapp.com/v1/historias/${id.split("=")[1]}`;
-        let a = false;
+        // let a = false;
+        let status;
         fetch(url, {
             headers: {
                 'Content-Type': 'application/json',
@@ -41,20 +57,42 @@ const MainHistoria = (props) => {
             },
             method: "DELETE",
         })
-        .then((res) => res.json())
-        .then((res) => {
-            console.log(res);
-            a = true;
-        }).catch((err) => {
-            console.log(err);
-        });
-        if (a) {
-            alert("Cambios realizados");
+        .then((res) => { 
+            status = res.status; 
+            return res.json() 
+        })
+        .then((jsonData) => {
+            // console.log("jsonData", jsonData);
+            // console.log("status", status);
+            // a = true;
+            alert("Eliminación realizada exitosamente");
             historia.replace("/")
-        }
-        if (!a) {
-            alert("Cambios no realizados, no eres administrador o autor de la historia");
-        }
+            return true;
+        })        
+        .catch((err) => {
+            if(status === 201 || status === 200) {
+                // a = true;
+                console.log("status", status);
+            } 
+            console.log(err);
+            alert("Eliminación no realizada, no eres administrador o autor de la historia");
+        });
+
+
+        // .then((res) => res.json())
+        // .then((res) => {
+        //     console.log(res);
+        //     a = true;
+        // }).catch((err) => {
+        //     console.log(err);
+        // });
+        // if (a) {
+        //     alert("Cambios realizados");
+        //     historia.replace("/")
+        // }
+        // if (!a) {
+        //     alert("Cambios no realizados, no eres administrador o autor de la historia");
+        // }
     };
     useEffect(() => {
         entrar();
@@ -81,7 +119,13 @@ const MainHistoria = (props) => {
                                 <i className="fas fa-pencil-alt" onClick={cambiar}></i>
                             </span>
                             <span className="button is-danger is-outlined">
-                                <i className="fas fa-trash" onClick={eliminar}></i> </span>
+                                <i className="fas fa-trash" onClick={eliminar}></i> 
+                            </span>
+                            <span className="button is-home is-link is-outlined">
+                                <i className="fas fa-home" onClick={regresar}></i>
+                            </span>
+                            <div className="columns is-centered">
+                            </div>
                         </>
                     }
                 </div>
@@ -101,6 +145,7 @@ const MainHistoria = (props) => {
                     </TextTagsMain>
                 </div>
             </div>
+
             <div className="columns is-centered">
                 <div className="column is-8">
                     <TextTags id="tags" />
--- src/components/historia/index1.js
+++ src/components/historia/index1.js
@@ -2,10 +2,18 @@ import React, { useEffect } from "react";
 import { useHistory } from 'react-router-dom';
 import Header from "../Header/index3";
 import { GlobalStyle } from "../../GlobalStyle";
+// import FormSimple from "../FormSimple";
+
 // Assets
-import { TextMain, TextEdit, TextTags, TextTagsMain, ContenedorText, TextSub } from "../../GlobalStyle";
+//, TextInput
+//TextMain,
+//, TextSub
+import { TextEdit, TextTags, TextTagsMain, ContenedorText } from "../../GlobalStyle";
 
 const MainHistoria = (props) => {
+    let let_titulo = "";
+    let let_tematica = "";
+    let let_texto = "";
     const datos =  () => {
         let username = document.cookie.split('; ').find(row => row.startsWith('user='))
         if (username) {
@@ -20,9 +28,11 @@ const MainHistoria = (props) => {
         datos();
     });
     let historia = useHistory();
-    const cambiar = () => {
+    const cambiar_pagina = () => {
+        // console.log("Cambiar pagina");
         let busq = historia.location.pathname.split("/")[2];
         historia.push(`/historia/${busq}/`);
+        return true;
     }
     let token = document.cookie.split(';').find(row => row.trim().startsWith('token='));
     let id = document.cookie.split(';').find(row => row.trim().startsWith('id='))
@@ -32,17 +42,26 @@ const MainHistoria = (props) => {
         const url = `https://historiasdeterror.herokuapp.com/v1/historias/${busq}`;
         const titulo = document.getElementById("titulo");
         const tematica = document.getElementById("tematica");
+        // console.log("tematica", tematica);
+        let_titulo =  titulo;
+        let_tematica = tematica;
         const texto = document.getElementById("texto");
         const tags = document.getElementById("tags");
         // console.log(cuerpo);
         let result = {};
-        console.log(url)
+        // console.log(url)
         fetch(url)
         .then((res) => res.json())
         .then((res) => {
+            // console.log("Entro a asignar datos");
             result = res["0"];
             titulo.innerHTML = result.titulo;
             tematica.innerHTML = result.tematica;
+            tematica.value = result.tematica;
+            titulo.value = result.titulo;
+            let_titulo =  result.titulo;
+            let_tematica = result.tematica;
+            let_texto = result.texto;
             texto.innerHTML = result.texto;
             tags.innerHTML = result.tag.slice(1, result.tag.length - 1).split(',');
         }).catch((err) => {
@@ -56,29 +75,59 @@ const MainHistoria = (props) => {
     let enviar = async function () {
         const url = `https://historiasdeterror.herokuapp.com/v1/historias/${id.split("=")[1]}`;
         let cuerpo = { "texto": document.getElementById("texto").value };
-        console.log(cuerpo)
-        let a = false;
-        fetch(url, {
-            method: "PUT",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': "Bearer " + token.split("=")[1],
-            },
-            body: JSON.stringify(cuerpo),
-        })
-        .then((res) => res.json())
-        .then((res) => {
-            console.log(res);
-            a = true;
-        }).catch((err) => {
-            console.log(err);
-        });
-        if (a) {
-            alert("Cambios realizados");
-            historia.replace("/")
-        }
-        if (!a) {
-            alert("Cambios no realizados, no eres administrador o autor de la historia");
+        let tematica2 = { "tematica": document.getElementById("tematica").value };
+        let titulo2 = { "titulo": document.getElementById("titulo").value };
+        // console.log(cuerpo)
+        // let a = false;
+        if (let_titulo !== titulo2.titulo || let_tematica !== tematica2.tematica || let_texto !== cuerpo.texto) 
+        {
+            let bodyData = {
+                'titulo': titulo2.titulo,
+                'tematica': tematica2.tematica,
+                'texto': cuerpo.texto
+            }
+            // console.log("bodyData", bodyData);
+            let status;
+            fetch(url, {
+                method: "PUT",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': "Bearer " + token.split("=")[1],
+                },
+                body: JSON.stringify(bodyData),
+            })
+            // .then((res) => res.json())
+            .then((res) => { 
+                status = res.status; 
+                return res.json() 
+            })
+            .then((jsonData) => {
+                // console.log("jsonData", jsonData);
+                // console.log("status", status);
+                // a = true;
+                alert("Cambios realizados");
+                historia.replace("/")
+                return true;
+            })        
+            // .then((res) => {
+            //     console.log(res);
+            //     a = true;
+            // })
+            .catch((err) => {
+                if(status === 201 || status === 200) {
+                    // a = true;
+                    console.log("status", status);
+                } 
+                console.log(err);
+                alert("Cambios no realizados, no eres administrador o autor de la historia");
+            });
+            // if (a) {
+            //     alert("Cambios realizados");
+            //     historia.replace("/")
+            // }
+            // if (!a) {
+            //     alert("Cambios no realizados, no eres administrador o autor de la historia");
+            // }    
         }
     }
 
@@ -86,6 +135,8 @@ const MainHistoria = (props) => {
     useEffect(() => {
         entrar();
     });
+    
+     
     return (
         <>
             <Header />
@@ -93,12 +144,39 @@ const MainHistoria = (props) => {
                 <div className="columns is-centered">
                     <div className="column is-7">
                         <div className="is-narrow">
-                            <TextMain id="titulo">
+                            {/* <TextMain id="titulo">
                                 ID: {props.show}
-                            </TextMain>
-                            <TextSub id="tematica">
+                            </TextMain> */}
+                            {/* <TextSub id="tematica">
                                 Tematica
-                            </TextSub>
+                            </TextSub> */}
+                            {/* <TextInput id="titulo" className="is-narrow">
+                            ID: {props.show}
+                            </TextInput> */}
+
+                            <input type="text"
+                            id="titulo"   
+                            className="form-control" 
+                            placeholder="Título" 
+                            defaultValue={let_titulo}  
+                            size="60"
+                            required
+                            autoFocus
+                            />
+                        </div>
+                        <div className="is-narrow">
+                            <input type="text"
+                            id="tematica"   
+                            className="form-control" 
+                            placeholder="Temática" 
+                            defaultValue={let_tematica}  
+                            size="60"
+                            required
+                            />
+
+                            {/* <TextInput id="tematica" className="is-narrow">
+                            tematica
+                            </TextInput> */}
                         </div>
                     </div>
                     <div className="is-2">
@@ -107,7 +185,7 @@ const MainHistoria = (props) => {
                         </span>
                         &nbsp;
                         <span className="button is-danger is-outlined">
-                            <i className="fas fa-times" onClick={cambiar}></i>
+                            <i className="fas fa-times" onClick={cambiar_pagina}></i>
                         </span>
                     </div>
                 </div>
